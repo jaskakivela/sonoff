61e16f798877b14cac33828f535a9eabaa46a019
Jaska Kivelä <jaska@kivela.net>
2018-11-10 11:08:01 +0000

    Add Sonoff configurator build

---
+++ sonoff-server/configure/configure.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "fmt"
+    "io"
+    "net/http"
+    "log"
+    "os"
+)
+
+func SonoffConfigServer(w http.ResponseWriter, req *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    w.Write([]byte('{"error": 0,"reason": "ok","IP": "192.168.0.16","port": "8443"}'))
+    // fmt.Fprintf(w, "This is an example server.\n")
+    // io.WriteString(w, "This is an example server.\n")
+}
+
+func main() {
+    http.HandleFunc("/dispatch/device", SonoffConfigServer)
+    port := os.Getenv("PORT")
+    if len(port) == 0 {
+        port = "8442"
+    }
+
+    err := http.ListenAndServeTLS(":"+port, "server.crt", "server.key", nil)
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
+}
